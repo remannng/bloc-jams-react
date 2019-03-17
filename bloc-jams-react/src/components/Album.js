226b3319dd0fbb9f05b0769800a79f5499a05d38
Ryan Manning <ryan.edward.manning@gmail.com>
2019-03-17 18:39:04 -0500

    Assignment 7 Bloc Jams-Audio Playback: Completed

--- bloc-jams-react/src/components/Album.js
+++ bloc-jams-react/src/components/Album.js
@@ -63,8 +63,21 @@ class Album extends Component {
            </colgroup>
            <tbody>
            {this.state.album.songs.map( (song, index) =>
-            <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
-              <td className="song-number">{'Track'+ " " + (index+1)}</td>
+            <tr className="song" key={index} onClick={() => this.handleSongClick(song)}
+              onMouseEnter={() => this.setState({ isHovered: index + 1 })}
+              onMouseLeave={() => this.setState({ isHovered: false })}>
+              <td className="song-actions">
+                <button>
+                  { (this.state.currentSong.title === song.title) ?
+                    <span className={this.state.isPlaying ? "ion-pause" : "ion-play"}></span>
+                    :
+                    (this.state.isHovered === index+1) ?
+                    <span className="ion-play"></span>
+                    :
+                    <span className="song-number">{index+1}</span>
+                  }
+                </button>
+              </td>
               <td className="song-title">{song.title}</td>
               <td className="song-duration">{song.duration}</td>
             </tr>
